8659db71f0e400c494091f84e311dd81c51c2b1d
TomoeAt <a-belle.k@live.de>
2019-09-17 11:22:04 +0200

    Version 17.Sept.

--- Datenbank auslesen.py
+++ Datenbank auslesen.py
@@ -9,6 +9,11 @@ class Anmeldung_an_Datenbank:
         self.password = Password
         self.database = Datenbank
 
+    def myLogging(self, logtext):
+        file = open('log.txt', 'a')
+        file.write(logtext)
+        file.close
+
     def nutzer_anlegen(self,test):
         # username = username aus Datenbank muss unten dann eingefuegt werden
         # test = new_user  # input("Benutzername:")
@@ -16,19 +21,26 @@ class Anmeldung_an_Datenbank:
         os.system("useradd -d /home/" + test + " -m " + test + " -p " + new_pwd)
         print(test)
         # Benutzer wird angelegt Passwort wird auch angelegt
+        logtext= 'Benutzer erfolgreich anglegt.\n'
+        self.myLogging(logtext)
 
 
     def Anmelden(self):
         mariadb_connection = mariadb.connect(user=self.user, password=self.password, database=self.database)
         cursor = mariadb_connection.cursor()
+        logtext = 'Erfolgreich an Datenbank angemeldet.\n'
+        self.myLogging(logtext)
         cursor.execute("SELECT * FROM Benutzer")
         result = cursor.fetchall()
         l =len(result)
         for r in result:
           test = r[1]
           self.nutzer_anlegen(test)
+          logtext = 'Daten erfolgreich ausgelesen \n'
+          self.myLogging(logtext)
 
-        mariadb_connection.close()
+          mariadb_connection.close()
+
 
 
 # Nutzerdateneingabe
